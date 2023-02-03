f2ae3a953825b1cd1028d19024c6e3dfdc3b1b74
Altair <dino.ahmetbegovic@gmail.com>
2023-02-03 21:11:15 +0100

    Update

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from colorama import init
-from termcolor import colored
-from pyfiglet import figlet_format
+from colorama import init  # Imports init function from colorama library
+from termcolor import colored  # Imports colored function from termcolor library
+from pyfiglet import figlet_format  # Imports figlet_format function from pyfiglet library
 
-init()
+init()  # Initializes colorama library
 
 
+# Function to check if color input is valid and return the valid color
 def color_check(color):
-    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
-    while color not in valid_colors:
+    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")  # List of valid colors
+    while color not in valid_colors:  # Loop until a valid color is inputted
         print(f"\n{color} is not valid. Please choose another color.")
         print("These are the valid colors: ", end="")
         for e in valid_colors:
@@ -16,19 +17,20 @@ def color_check(color):
     return color
 
 
+# Function to check if the user wants to include attributes
 def attribute_check():
-    answer = input("Would you like to include attributes? ")
-    while answer.lower() not in ("yes", "no"):
+    answer = input("Would you like to include attributes? ")  # Ask user if they want to include attributes
+    while answer.lower() not in ("yes", "no"):  # Loop until user inputs either "yes" or "no"
         print('The answer must be "yes" or "no".')
         answer = input("\nWould you like to include attributes? ")
 
-    if answer == "yes":
-        attributes = []
-        valid_attributes = ("bold", "dark", "underline", "blink", "reverse", "concealed")
-        while 1:
+    if answer == "yes":  # If user inputs "yes", proceed to input attributes
+        attributes = []  # List to store inputted attributes
+        valid_attributes = ("bold", "dark", "underline", "blink", "reverse", "concealed")  # List of valid attributes
+        while 1:  # Loop to input attributes
             attribute = input("What attribute would you like to use? ")
 
-            while attribute not in valid_attributes:
+            while attribute not in valid_attributes:  # Loop until user inputs a valid attribute
                 print(f"\n{attribute} is not valid. Please choose another attribute.")
                 print("These are the valid attribute:", end="")
                 for e in valid_attributes:
@@ -36,38 +38,41 @@ def attribute_check():
                 attribute = input("\nWhat attribute would you like to use? ")
             attributes.append(attribute)
 
-            answer = input("\nWould you like to add another attribute? ")
-            while answer.lower() not in ("yes", "no"):
+            answer = input(
+                "\nWould you like to add another attribute? ")  # Ask user if they want to add another attribute
+            while answer.lower() not in ("yes", "no"):  # Loop until user inputs either "yes" or "no"
                 print('The answer must be "yes" or "no".')
                 answer = input("\nWould you like to include attributes? ")
 
-            if answer == "yes":
+            if answer == "yes":  # If user inputs "yes", loop back to input another attribute
                 continue
-            break
+            break  # If user inputs "no", exit loop
 
-        return set(attributes)
-    return None
+        return set(attributes)  # Return the list of attributes as a set to eliminate duplicates
+    return None  # If user inputs "no", return None
 
 
+# Function to check if the user wants to include a background color and return the valid background color
 def on_color_check():
-    answer = input("Would you like to include color background? ")
-    while answer.lower() not in ("yes", "no"):
+    answer = input(
+        "Would you like to include color background? ")  # Ask user if they want to include a background color
+    while answer.lower() not in ("yes", "no"):  # Validate the user's answer
         print('The answer must be "yes" or "no".')
         answer = input("\nWould you like to include color background? ")
 
-    if answer == "yes":
-        valid_backgrounds = ("on_red", "on_green", "on_yellow", "on_blue", "on_magenta", "on_cyan", "on_white")
-        background = input("What background would you like to use? ")
+    if answer == "yes":  # If the user wants to include a color background, ask for the background color
+        valid_backgrounds = ("on_red", "on_green", "on_yellow", "on_blue", "on_magenta", "on_cyan", "on_white")  # List of valid background colors
+        background = input("What background would you like to use? ")   # Ask the user for the background color
 
-        while background not in valid_backgrounds:
+        while background not in valid_backgrounds:  # Validate the user's answer
             print(f"\n{background} is not valid. Please choose another background.")
             print("These are the valid background:", end="")
             for e in valid_backgrounds:
                 print("  ", e, end="")
             background = input("\nWhat background would you like to use? ")
 
-        return background
-    return None
+        return background   # Return the background color
+    return None  # If the user does not want to include a color background, return None
 
 
 def print_art(message, color):
